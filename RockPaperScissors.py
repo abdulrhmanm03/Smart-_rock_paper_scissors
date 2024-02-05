07ce4ad348473f3f1ff42f4e4ce3810d910e1205
abdulrhman mustafa <146654601+abdulrhmanm03@users.noreply.github.com>
2024-02-05 03:18:05 +0200

    Add files via upload

---
+++ RockPaperScissors.py
@@ -0,0 +1,62 @@
+import random
+
+
+
+played_after = {"r": {"r": 0, "p": 0, "s": 0, "t": 0}, "p": {"r": 0, "p": 0, "s": 0, "t": 0}, "s": {"r": 0, "p": 0, "s": 0, "t": 0}}
+
+def addafter(xi, x, dict):
+    dict[x]["t"] += 1 
+    dict[xi][x] += 1
+    
+def choice(r,p,s,t):
+    if r == p == s == 0:
+        return random.choice(['r', 'p', 's'])   
+    wp = (r/t)*100
+    ws = (p/t)*100
+    wr = (s/t)*100
+    return random.choices(['r', 'p', 's'], weights = (wr, wp, ws))[0]
+          
+xi = D = L = W = 0
+
+while True:
+    x = ''  
+    
+    while True:
+        print(" ")
+        x = input("'r' for rock, 'p' for paper, 's' for sissor, 'x' to close: ")
+        print(" ")
+        x = x.lower().strip()
+        if x in ['r', 'p', 's', 'x']:
+            break
+        print("enter a valid input")
+    
+    if x == 'x':
+        break
+    
+    if not xi:
+        y = random.choice(['r', 'p', 's'])
+        xi = x
+        played_after[x]["t"] += 1
+    else:
+        y = choice(played_after[xi]['r'], played_after[xi]['p'], played_after[xi]['s'], played_after[xi]["t"])
+        addafter(xi, x, played_after)
+        xi = x       
+    
+    if x == y :
+        print('    draw')
+        print("-------------")
+        D +=1    
+    else :
+        if (x == 'r' and y == 'p') or (x == 'p' and y == 's') or (x == 's' and y == 'r'):
+            print('  You Loss')
+            print("-------------")
+            L +=1       
+        else :
+            print('  You Win')
+            print("-------------")
+            W +=1    
+
+print("---------------------------------")
+print(f"      W: {W} || D: {D} || L: {L}")  
+print("---------------------------------")                             
+ 
